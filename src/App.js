635c14808dcc5763f5741003a31ff49e727acfae
JS16541 <JS16541@jeenasikholifecare.com>
2025-01-07 18:34:29 +0530

    FUNTCTION

--- src/App.js
+++ src/App.js
@@ -4,19 +4,32 @@ import Pagination from "./components/Pagination"
 import { useContext, useEffect } from "react";
 import { AppContext } from "./components/context/AppContext";
 import "./App.css"
-import { Route, Router } from "react-router-dom";
+import { Route, Router, useLocation, useSearchParams } from "react-router-dom";
 
 export default function App() {
-  const {fetchBlogPosts} =useContext(AppContext)
+  const {fetchBlogPosts} =useContext(AppContext);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
   useEffect(()=>{
-    fetchBlogPosts();
-  },[]);
+    const page =searchParams.get("page") ?? 1;
+    console.log(page);
+    if(location.pathname.includes("tags")){
+      const tag = location.pathname.split("/").at[-1].replaceAll("-"," ") ; 
+      fetchBlogPosts(Number(page),tag); 
+    }else if(location.pathname.includes("category")){
+      const category = location.pathname.split("/").at(-1).replaceAll("-"," ");
+      fetchBlogPosts(Number(page),category);
+    }else{
+      fetchBlogPosts(Number(page));
+    }
+  },[location.pathname, location.search]);
   return (
     <Routes>
-      <Route Path="/" element={<Home/>}/>
-      <Route Path="/blog/:blogId" element={<BlogPage/>}/>
-      <Route Path="/tag/:tag" element={<TagPage/>}/>
-      <Route Path="/category/:category" element={<CategoryPage/>}/>
-   </Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/blog/:blogId" element={<BlogPage />} />
+      <Route path="/tag/:tag" element={<TagPage />} />
+      <Route path="/category/:category" element={<CategoryPage />} />
+    </Routes>
+
   )
 }
---
+++ src/Pages/TagPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const tagPage = () => {
+  return (
+    <div>tagPage</div>
+  )
+}
